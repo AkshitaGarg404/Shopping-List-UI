286cbdc9b869571419fff8230dcb9ca7320b4ea6
AkshitaGarg404 <151184393+AkshitaGarg404@users.noreply.github.com>
2025-06-22 04:26:01 +0530

    Local Storage integrated and all functions modified to incorporate it

--- script.js
+++ script.js
@@ -4,27 +4,20 @@ const itemList=document.querySelector('#item-list')
 const itemInput=document.querySelector('#item-input')
 const filter=document.querySelector('#filter')
 const clearAll=document.querySelector('#clear')
+//Getting items from storage:
+function getItemsFromLocal(){
+    return JSON.parse(localStorage.getItem('items'))||[];  //efficiently using or
+}
 
 //✅Functionality: Add Items to the list
 //form element: itemForm
-function addItem(e){
-    //Prevent form from submitting first
-    e.preventDefault();
+function addItemUI(item){
     //Create a new list item
     const li=document.createElement('li')
     //assign it class name for css : NO classes
     //Add everything in: 
-
-    //Text node: use FormData to get submitted value: using name attribute
-    const formData=new FormData(itemForm)
-    //Validate input
-    if(itemInput.value===""){ //or formData.get('item')
-        alert("Please enter a valid item")
-        return; //very important coz otherwise it will keep on executing outside the if too
-    }
-    const text=document.createTextNode(formData.get('item')) //Its better to put formData.get('item') in a variable since we are using it again and again
+    const text=document.createTextNode(item) //Its better to put formData.get('item') in a variable since we are using it again and again
     li.appendChild(text)
-
     //Button
     const button=document.createElement('button')
     //add classes
@@ -34,50 +27,130 @@ function addItem(e){
     //set classes
     cross.setAttribute('class','fa-solid fa-xmark')
     button.appendChild(cross)
-
     li.appendChild(button)
-
     itemList.appendChild(li);
-
-    itemInput.value=''// very necessary to clear Enter item
-
-    unhide(); //Unhide filter and clearAll
 };
 
+function addItemToLocal(item){
+    const items=getItemsFromLocal();
+    items.push(item)
+    localStorage.setItem('items',JSON.stringify(items))
+}
 
-//✅Functionality: Remove Item from the list on clicking cross
-function removeItem(e){
-    //1: Better
+function addItem(e){
+    //Prevent form from submitting first
+    e.preventDefault();
+    //use FormData to get submitted value: using name attribute
+    const formData=new FormData(itemForm)
+    const item=formData.get('item')
+    //Validate input
+    if(item===""){
+        alert("Please enter a valid item")
+        return; //very important coz otherwise it will keep on executing outside the if too
+    }
+    //Creating UI item, clearing input field, and unhiding filter and clear all
+    addItemUI(item);
+    itemInput.value=''// very necessary to clear Enter item
+    unhide(); //Unhide filter and clearAll
+    //Adding to Storage
+    addItemToLocal(item);
+}
+
+
+
+//✅Functionality: Display items from Local Storage at the time of reload
+function displayItems(){
+    const items=getItemsFromLocal();
+    if(items.length===0){
+        hide();
+    } else {
+        items.forEach(item=>{
+            addItemUI(item);
+        });
+        unhide();
+    }
+    
+}
+
+
+//✅Functionality: Function for clicking anywhere on li.. it updates or deletes depending on where you clicked
+function clickOnItem(e){
+    //Checking if user clicked on cross
+    // if(e.target.parentElement.classList.contains('remove-item'))
     if(e.target.closest('.remove-item')){
-        const item=e.target.closest('li')
-        if(item){
-            if(confirm(`Are you sure you want to remove '${item.innerText}' from the list`))
-            item.remove();
-            //to hide buttons and filters if there are no elements
-            if(!document.querySelector('#item-list li'))    hide()
+        removeItem(e.target);
+    }
+}
+
+
+
+//✅Functionality: Remove Item from the list UI
+function removeItemUI(item){    
+    item.remove();
+    //to hide buttons and filters if there are no elements
+    if(!document.querySelector('#item-list li'))    hide()   
+}
+//✅Functionality: Remove Item from local storage
+function removeItemFromLocal(item){
+    const itemName=item.innerText    
+    let items=getItemsFromLocal();
+    items=items.filter(thing=>thing!==itemName)
+    localStorage.setItem('items',JSON.stringify(items));
+}
+//✅Functionality: Overall Remove Item function
+function removeItem(eleClicked){
+    // e.target.parentElement.parentElement.remove();
+    const item=eleClicked.closest('li')
+    if(item){
+        if(confirm(`Are you sure you want to remove '${item.innerText}' from the list`)){
+            removeItemUI(item);
+            removeItemFromLocal(item);
         }
     }
-    //2: not that readable
-    // if(e.target.parentElement.classList.contains('remove-item')){
-    //     e.target.parentElement.parentElement.remove();
-    // }
 }
+
+ 
+
 
 // ✅Functionality: Clear All items: just set innerHTML of ul as empty
 function onClear(){
-    // 1:direct
-    // itemList.innerHTML=""
-
-    //2: using loop
+    // itemList.innerHTML=""  //simple
+    // using loop
     if(confirm("Are you sure you want to clear the list?"))
     { 
+        //Removing UI
         while(itemList.firstChild){
             itemList.removeChild(itemList.firstChild)
         }
         //now hide all buttons
         hide()
+
+        //now clear local storage
+        // localStorage.clear(); //don't do this as other key value pairs will also be deleted
+        localStorage.removeItem('items') //better and safer
     }
 }
+
+
+
+//✅Functionality: Filter Items
+function filterItems(e){
+    const val=e.target.value.toLowerCase()
+    const items=document.querySelectorAll('#item-list li')
+    items.forEach(item=>{
+        //const text=item.firstChild.textContent is better as innerText is slower than textContent
+        //if(str.indexOf(val)!==-1)
+        if(item.innerText.toLowerCase().includes(val)){
+            //item.style.display='flex'
+            item.classList.remove('hidden')
+        } else {
+            //item.style.display='none'
+            item.classList.add('hidden')
+        }
+    });
+}
+
+
 
 //💅🏻Styling: making sure that filter items and clear all are not visible till there is atleast one item in the list
 function hide(){
@@ -101,32 +174,20 @@ function checkUI(){
 }
 
 
-//✅Functionality: Filter Items
-function filterItems(e){
-    const val=e.target.value.toLowerCase()
-    const items=document.querySelectorAll('#item-list li')
-    items.forEach(item=>{
-        //const text=item.firstChild.textContent is better as innerText is slower than textContent
-        //if(str.indexOf(val)!==-1)
-        if(item.innerText.toLowerCase().includes(val)){
-            //item.style.display='flex'
-            item.classList.remove('hidden')
-        } else {
-            //item.style.display='none'
-            item.classList.add('hidden')
-        }
-    });
+//Event Listeners: initialise app function()
+function init(){
+    //adding item
+    itemForm.addEventListener('submit', addItem)
+    //On DOM load... call initial display from local storage
+    window.addEventListener('DOMContentLoaded',()=>{
+        displayItems();
+    })
+    //Event Delegation: add remove item to parent
+    itemList.addEventListener('click',clickOnItem)
+    //Clear all
+    clearAll.addEventListener('click',onClear)
+    //Filtering
+    filter.addEventListener('input',filterItems)
 }
 
-//Event Listeners
-//adding item
-itemForm.addEventListener('submit', addItem)
-//Event Delegation: add remove item to parent
-itemList.addEventListener('click',removeItem)
-//Clear all
-clearAll.addEventListener('click',onClear)
-//On DOM load... see whether filter and clearAll should be displayed or not
-window.addEventListener('DOMContentLoaded',()=>{
-    checkUI();
-})
-filter.addEventListener('input',filterItems)
+init();
